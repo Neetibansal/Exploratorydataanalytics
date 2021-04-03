7685b4defda566e395cda83322f82b9e05be6f48
Neetibansal <81407329+Neetibansal@users.noreply.github.com>
2021-04-03 20:54:25 +0530

    Update plot-3.r

--- plot-3.r
+++ plot-3.r
@@ -3,25 +3,25 @@ library("data.table")
 setwd("C:/Users/HP/Desktop/Exploratorydataanalytics")
 
 #Reads in data from file 
-powerDT <- data.table::fread(input = "household_power_consumption.txt"
+PDT <- data.table::fread(input = "household_power_consumption.txt"
                              , na.strings="?"
 )
 
 # Prevents Notation
-powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+PDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
 
 # Making a POSIXct date capable of being filtered 
-powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+PDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
 
 # Filter Date
-powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+PDT <- PDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
 
 png("plot3.png", width=480, height=480)
 
 # Plot 3
-plot(powerDT[, dateTime], powerDT[, Sub_metering_1], type="l", xlab="", ylab="Energy sub metering")
-lines(powerDT[, dateTime], powerDT[, Sub_metering_2],col="red")
-lines(powerDT[, dateTime], powerDT[, Sub_metering_3],col="blue")
+plot(PDT[, dateTime], PDT[, Sub_metering_1], type="l", xlab="", ylab="Energy sub metering")
+lines(PDT[, dateTime], PDT[, Sub_metering_2],col="red")
+lines(PDT[, dateTime], PDT[, Sub_metering_3],col="blue")
 legend("topright"
        , col=c("black","red","blue")
        , c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  ")
